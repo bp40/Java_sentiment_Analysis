3d37d88d3e9db754f5ac61dda80c45d7502d9501
Bhutas Polpanich <bhutas.pol@student.mahidol.edu>
2020-11-20 15:33:19 +0000

    proof of concept for grabbing links

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
 import java.io.IOException;  
 import org.jsoup.Jsoup;  
 import org.jsoup.nodes.Document; 
+import org.jsoup.nodes.Element;
 
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException{
     
+    final String mainUrl = "https://forums.macrumors.com/forums/macos-big-sur-11-0.226/";
+
+    try {
+
+      final Document doc = Jsoup.connect(mainUrl).get();
+
+      for (Element links : doc.select("div.structItem-title a[href]")) {
+        System.out.println("https://forums.macrumors.com/" + links.attr("href"));
+      }
+
+    } catch (Exception except) {
+      System.out.println(except);
+      except.printStackTrace();
+    }
+
   }
 }
